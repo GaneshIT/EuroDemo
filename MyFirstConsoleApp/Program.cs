d0c9e9c3a234f5c7524d1b7e0c93c0e05694a791
Ganesan Perumalsamy <Ganesan.Perumalsamy@dell.com>
2021-09-13 17:53:24 +0530

    added console appln

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? value1=null,value2,value3;
+            Console.WriteLine("Enter value1:");
+            value1 =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value2:");
+            value2 = Convert.ToInt32(Console.ReadLine());
+            value3 = value1 + value2;
+
+            Console.WriteLine("result:"+ value3);
+            Console.Read();
+        }
+    }
+}
