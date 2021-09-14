1e66046e703c96d96a21a77ac9c2bf74c6441afd
Ganesan Perumalsamy <Ganesan.Perumalsamy@dell.com>
2021-09-14 12:54:48 +0530

    updated files

---
+++ MyFirstConsoleApp/FileHandling.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyFirstConsoleApp
+{
+    class FileHandling
+    {
+        // no retrun - void
+        // return - int, string, flot,  double, decimal,etc.
+        //acces specifiers: public,private,internal,protected, protected internal
+       public string WriteFile()
+        {
+            FileStream fs = new FileStream(@"C:\Training\EuroTraining\Files\temp.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine("Welcome to training");
+            sw.WriteLine("*******************");
+            sw.WriteLine("Java");
+            sw.WriteLine("C#");
+            sw.WriteLine("Javascript");
+            sw.WriteLine("HTML5");
+            sw.Close();
+            fs.Close();
+            return "File created";
+        }
+       public string ReadFile()
+        {
+            string[] files = Directory.GetFiles(@"C:\Training\EuroTraining\Files");
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileStream fs = new FileStream(files[i], FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                while (sr.Peek() > 0)
+                {
+                    string line = sr.ReadLine();
+                    if (line.Contains(":"))
+                    {
+                        string[] courses = line.Split(':');
+                        for (int j = 0;  j< courses.Length; j++)
+                        {
+                            Console.WriteLine(courses[j]);
+                        }
+                        Console.WriteLine("************");
+                        //Console.WriteLine(line);
+                    }
+                    
+                }
+                fs.Close();
+            }
+            
+            return "Read success";
+        }
+    }
+}
+/*
+Course details:
+enter number courses: 2
+
+Enter regno: 123
+enter course:BE
+enter course title:java
+
+Enter regno: 124
+enter course:BE
+enter course title:C#
+
+File:
+Course details
+*************
+Number of courses:2
+RegNo:123
+course:BE
+title:java
+
+RegNo:124
+course:BE
+title:c#
+
+
+Mobile Network performance
+*****************************
+Source      Destination      Date     call-status
+867334343   3243243423      23:2:2021   Missed
+
+
+
+
+
+
+
+ */
--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -12,14 +12,31 @@ namespace MyFirstConsoleApp
     {
         static void Main(string[] args)
         {
-            int? value1=null,value2,value3;
-            Console.WriteLine("Enter value1:");
-            value1 =Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter value2:");
-            value2 = Convert.ToInt32(Console.ReadLine());
-            value3 = value1 + value2;
 
-            Console.WriteLine("result:"+ value3);
+            FileHandling objFile = new FileHandling();
+
+             //string msg= objFile.WriteFile();
+
+            //Console.WriteLine(msg);
+            Console.WriteLine("***********");
+            Console.WriteLine(objFile.ReadFile());
+            //string a = null;
+            //Console.WriteLine(Convert.ToInt32(a));
+            //Convert.ToString(objFile.ReadFile());
+            //Console.WriteLine("WriteFile: " + objFile.WriteFile());
+            //Console.WriteLine("ReadFile: " + objFile.ReadFile().ToString());
+            //int value, value2, value3;
+            //Console.WriteLine("Enter value1:");
+            //value = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter value2:");
+            //value2 = Convert.ToInt32(Console.ReadLine());
+            //value3 = value + value2;
+
+            //Console.WriteLine("addition:" + (value + value2));
+            ////20   30      2030
+            ////ctrl k+c
+            ////ctrl k+u
+            ///
             Console.Read();
         }
     }
